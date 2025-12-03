dd892c3b60a1887b8fb2384f8014b54a1c1ff3dd
HugoPelletie <hugopelletier83@gmail.com>
2025-12-03 16:36:09 +0100

    vFichedepaye

--- src/Controleurs/c_validationFicheDeFrais.php
+++ src/Controleurs/c_validationFicheDeFrais.php
@@ -1,6 +1,27 @@
 <?php
 
-use Outils\Utilitaires;
+include_once 'src/Modeles/PdoGsb.php';
 
+$pdo = PdoGsb::getPdoGsb();
+$action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING);
 
-include PATH_VIEWS . 'v_validationFicheDeFrais.php';
+switch ($action) {
+
+    case 'selectionFiche':
+        $lesVisiteurs = $pdo->getTousLesVisiteurs();
+        $lesMois = $pdo->getLesMoisDisponibles();
+        include 'src/Vues/v_selectionFicheValidation.php';
+        break;
+
+        case 'chargerFiche':
+        $idVisiteur = $_POST['visiteur'];
+        $mois = $_POST['mois'];
+
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
+        $etatFiche = $pdo->getEtatFiche($idVisiteur, $mois);
+
+        include 'src/Vues/v_validationFiche.php';
+        break;
+
+ 
